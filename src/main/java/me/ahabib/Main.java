89ce5f3a31467c00d2ee397193df62cd7da59db9
ahmed <65723807+adel-habib@users.noreply.github.com>
2023-03-25 11:44:46 +0100

    Access control and audit logging

--- src/main/java/me/ahabib/Main.java
+++ src/main/java/me/ahabib/Main.java
@@ -1,10 +1,14 @@
 package me.ahabib;
 
 import com.google.common.util.concurrent.RateLimiter;
+import me.ahabib.controller.AuditController;
+import me.ahabib.controller.ModeratorController;
 import me.ahabib.controller.SpaceController;
 import me.ahabib.controller.UserController;
 import org.dalesbred.Database;
+import org.dalesbred.result.EmptyResultException;
 import org.h2.jdbcx.JdbcConnectionPool;
+import org.json.JSONException;
 import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
@@ -17,24 +21,30 @@ import java.util.EmptyStackException;
 import static spark.Spark.*;
 
 public class Main {
+
     public static void main(String... args) throws Exception {
-
-
-        JdbcConnectionPool dataSource = JdbcConnectionPool.create("jdbc:h2:mem:natter", "natter", "password");
-        Database database = Database.forDataSource(dataSource);
+        secure("localhost.p12", "changeit", null, null);
+        var datasource = JdbcConnectionPool.create(
+                "jdbc:h2:mem:natter", "natter", "password");
+        var database = Database.forDataSource(datasource);
         createTables(database);
-        dataSource = JdbcConnectionPool.create("jdbc:h2:mem:natter", "natter_api_user", "password");
+        datasource = JdbcConnectionPool.create(
+                "jdbc:h2:mem:natter", "natter_api_user", "password");
+
+        database = Database.forDataSource(datasource);
         var spaceController = new SpaceController(database);
         var userController = new UserController(database);
+        var auditController = new AuditController(database);
+
         var rateLimiter = RateLimiter.create(2.0d);
 
-        exception(IllegalArgumentException.class,Main::badRequest);
-        exception(EmptyStackException.class, ((e, request, response) -> {response.status(404);}) );
-        before(((request, response) -> {
-            if(!rateLimiter.tryAcquire()){
-                response.header("Retry-After", "2");
+        before((request, response) -> {
+            if (!rateLimiter.tryAcquire()) {
                 halt(429);
             }
+        });
+
+        before(((request, response) -> {
             if (request.requestMethod().equals("POST") &&
                     !"application/json".equals(request.contentType())) {
                 halt(415, new JSONObject().put(
@@ -42,8 +52,6 @@ public class Main {
                 ).toString());
             }
         }));
-
-        before(userController::authenticate);
 
         afterAfter((request, response) -> {
             response.type("application/json;charset=utf-8");
@@ -56,19 +64,62 @@ public class Main {
             response.header("Server", "");
         });
 
-        post("/users", userController::registerUser);
+        before(userController::authenticate);
+
+        before(auditController::auditRequestStart);
+        afterAfter(auditController::auditRequestEnd);
+
+        before("/spaces", userController::requireAuthentication);
         post("/spaces", spaceController::createSpace);
-        after((request, response) -> {response.type("application/json");});
-        internalServerError(new JSONObject().put("error", "internal server error").toString());
-        notFound(new JSONObject().put("error", "not found").toString());
+
+        // Additional REST endpoints not covered in the book:
+
+        before("/spaces/:spaceId/messages",
+                userController.requirePermission("POST", "w"));
+        post("/spaces/:spaceId/messages", spaceController::postMessage);
+
+        before("/spaces/:spaceId/messages/*",
+                userController.requirePermission("GET", "r"));
+        get("/spaces/:spaceId/messages/:msgId",
+                spaceController::readMessage);
+
+        before("/spaces/:spaceId/messages", userController.requirePermission("GET", "r"));
+        get("/spaces/:spaceId/messages", spaceController::findMessages);
+
+        before("/spaces/:spaceId/members",
+                userController.requirePermission("POST", "rwd"));
+        post("/spaces/:spaceId/members", spaceController::addMember);
+
+        var moderatorController = new ModeratorController(database);
+
+        before("/spaces/:spaceId/messages/*",
+                userController.requirePermission("DELETE", "d"));
+        delete("/spaces/:spaceId/messages/:msgId",
+                moderatorController::deletePost);
+
+        get("/logs", auditController::readAuditLog);
+        post("/users", userController::registerUser);
+
+        internalServerError(new JSONObject()
+                .put("error", "internal server error").toString());
+        notFound(new JSONObject()
+                .put("error", "not found").toString());
+
+        exception(IllegalArgumentException.class, Main::badRequest);
+        exception(JSONException.class, Main::badRequest);
+        exception(EmptyResultException.class,
+                (e, request, response) -> response.status(404));
+    }
+
+    private static void badRequest(Exception ex,
+                                   Request request, Response response) {
+        response.status(400);
+        response.body(new JSONObject().put("error", ex.getMessage()).toString());
     }
 
     private static void createTables(Database database) throws Exception {
-        Path path = Paths.get(Main.class.getResource("/schema.sql").toURI());
+        var path = Paths.get(
+                Main.class.getResource("/schema.sql").toURI());
         database.update(Files.readString(path));
-    }
-    private static void badRequest(Exception exception, Request request, Response response) {
-        response.status(400);
-        response.body("{\"error\": \"" + exception.getMessage() + "\"}");
     }
 }
